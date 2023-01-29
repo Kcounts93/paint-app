fc25445fbdb1dc502432c60a93aecddf6ab5357c
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-01-29 11:31:28 -0800

    assign js variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initial refercences
+
+let gridContainer = document.querySelector(".grid-container");
+let gridBtn = document.getELementbyId("submit-grid");
+let clearGridBtn = document.getElementById("clear-grid");
+let gridWidth = document.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorBtn = document.getElementById("color-btn");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-gridBtn");
+let widthValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
+// Events object
